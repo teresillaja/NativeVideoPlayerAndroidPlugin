06dcbb070a4c6989b3fb84db573905bab103ba7f
Teresa Jimenez Arreola <teresilla@gmail.com>
2013-10-29 16:39:46 +0000

    modifying plugin

--- www/VideoPlayer.js
+++ www/VideoPlayer.js
@@ -1,17 +1,12 @@
-cordova.define("com.orange.labs.plugins.videoplayer.VideoPlayer", function(require, exports, module) {var videoPlayerAndroid = {
-    createEvent: function(json,successCallback, errorCallback) {
- 			cordova.exec(
-            // success callback function
-             function() {
-            	successCallback();
-       		 },
-       		 function(){
-       		 	errorCallback();
-       		 }, // error callback function
-            'VideoPlayer', // mapped to our native Java class called "HttpRequest"
+var videoplayer=  {
+    createEvent: function(json, successCallback, errorCallback) {
+        cordova.exec(
+            successCallback, // success callback function
+            errorCallback, // error callback function
+            'VideoPlayer', // mapped to our native Java class called "Calendar"
             'play', // with this action name
-            [json]           // and this array of custom arguments to create our entry
-            );    
+            [json]  
+        );
     }
 }
-module.exports = videoPlayerAndroid;});
+module.exports = videoplayer;
